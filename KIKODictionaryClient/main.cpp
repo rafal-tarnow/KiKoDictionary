5f92bb12f07c03a3b1424528cae49a8133c085d7
Rafal Kruszyna <rafal.kruszyna@gmail.com>
2025-06-03 11:15:37 +0200

    working version

--- KIKODictionaryClient/main.cpp
+++ KIKODictionaryClient/main.cpp
@@ -7,97 +7,97 @@
 //#include <QQuickStyle>
 
 
-#include <QFile>
-#include <QSslCertificate>
-#include <QSslConfiguration>
+// #include <QFile>
+// #include <QSslCertificate>
+// #include <QSslConfiguration>
 
-bool setGlobalSslCertificate(const QString& certPath)
-{
-    // 1. Wczytaj certyfikat z pliku
-    QFile certFile(certPath);
-    if (!certFile.open(QIODevice::ReadOnly)) {
-        qDebug() << "Błąd: Nie można otworzyć pliku certyfikatu:" << certPath;
-        return false;
-    }
+// bool setGlobalSslCertificate(const QString& certPath)
+// {
+//     // 1. Wczytaj certyfikat z pliku
+//     QFile certFile(certPath);
+//     if (!certFile.open(QIODevice::ReadOnly)) {
+//         qDebug() << "Błąd: Nie można otworzyć pliku certyfikatu:" << certPath;
+//         return false;
+//     }
 
-    // Wczytaj certyfikat w formacie PEM
-    QSslCertificate certificate(&certFile, QSsl::Pem);
-    certFile.close();
+//     // Wczytaj certyfikat w formacie PEM
+//     QSslCertificate certificate(&certFile, QSsl::Pem);
+//     certFile.close();
 
-    if (certificate.isNull()) {
-        qDebug() << "Błąd: Certyfikat jest nieprawidłowy lub uszkodzony!";
-        return false;
-    }
+//     if (certificate.isNull()) {
+//         qDebug() << "Błąd: Certyfikat jest nieprawidłowy lub uszkodzony!";
+//         return false;
+//     }
 
-    // 2. Pobierz domyślną konfigurację SSL
-    QSslConfiguration sslConfig = QSslConfiguration::defaultConfiguration();
+//     // 2. Pobierz domyślną konfigurację SSL
+//     QSslConfiguration sslConfig = QSslConfiguration::defaultConfiguration();
 
-    // Dodaj certyfikat do listy zaufanych certyfikatów
-    QList<QSslCertificate> caCerts = sslConfig.caCertificates();
-    caCerts.append(certificate);
-    sslConfig.setCaCertificates(caCerts);
+//     // Dodaj certyfikat do listy zaufanych certyfikatów
+//     QList<QSslCertificate> caCerts = sslConfig.caCertificates();
+//     caCerts.append(certificate);
+//     sslConfig.setCaCertificates(caCerts);
 
-    // 3. Ustaw globalną konfigurację SSL
-    QSslConfiguration::setDefaultConfiguration(sslConfig);
+//     // 3. Ustaw globalną konfigurację SSL
+//     QSslConfiguration::setDefaultConfiguration(sslConfig);
 
-    qDebug() << "Globalna konfiguracja SSL ustawiona pomyślnie.";
-    return true;
-}
-
-
+//     qDebug() << "Globalna konfiguracja SSL ustawiona pomyślnie.";
+//     return true;
+// }
 
 
 
 
 
 
-void skonfigurujZaufanyCertyfikat() {
-    // 1. Załaduj swój certyfikat z zasobów
-    // QFile certFile("/home/rafal/fastapi_ssl/server.crt");
-    // if (!certFile.open(QIODevice::ReadOnly)) {
-    //     qWarning() << "Nie można otworzyć pliku certyfikatu z zasobów:" << certFile.errorString();
-    //     return;
-    // }
-
-    // // Odczytaj zawartość pliku
-    // QByteArray certData = certFile.readAll();
-    // certFile.close();
-
-    // Utwórz obiekt QSslCertificate
-    // QSslCertificate::fromData może zwrócić listę, jeśli plik PEM zawiera łańcuch
-    QList<QSslCertificate> certificates = QSslCertificate::fromPath(QStringLiteral(":/assets/server.crt"));
-    if (certificates.isEmpty()) {
-        qWarning() << "Nie udało się załadować certyfikatu lub plik jest pusty/nieprawidłowy.";
-        return;
-    }
-
-    // Możesz wziąć pierwszy lub wszystkie, jeśli twój plik .pem zawiera cały łańcuch
-    // Dla pojedynczego certyfikatu self-signed, wystarczy pierwszy.
-    QSslCertificate selfSignedCertificate = certificates.first();
-    // Lub jeśli chcesz dodać wszystkie z pliku:
-    // QList<QSslCertificate> selfSignedCertificates = certificates;
 
 
-    // 2. Pobierz domyślną konfigurację SSL
-    QSslConfiguration sslConfig = QSslConfiguration::defaultConfiguration();
+// void skonfigurujZaufanyCertyfikat() {
+//     // 1. Załaduj swój certyfikat z zasobów
+//     // QFile certFile("/home/rafal/fastapi_ssl/server.crt");
+//     // if (!certFile.open(QIODevice::ReadOnly)) {
+//     //     qWarning() << "Nie można otworzyć pliku certyfikatu z zasobów:" << certFile.errorString();
+//     //     return;
+//     // }
 
-    // 3. Pobierz listę aktualnie zaufanych certyfikatów CA
-    QList<QSslCertificate> caCertificates = sslConfig.caCertificates();
+//     // // Odczytaj zawartość pliku
+//     // QByteArray certData = certFile.readAll();
+//     // certFile.close();
 
-    // 4. Dodaj swój certyfikat do listy zaufanych CA
-    // Jeśli używasz QList<QSslCertificate> selfSignedCertificates:
-    // caCertificates.append(selfSignedCertificates);
-    // Dla pojedynczego certyfikatu:
-    caCertificates.append(selfSignedCertificate);
+//     // Utwórz obiekt QSslCertificate
+//     // QSslCertificate::fromData może zwrócić listę, jeśli plik PEM zawiera łańcuch
+//     QList<QSslCertificate> certificates = QSslCertificate::fromPath(QStringLiteral(":/assets/server.crt"));
+//     if (certificates.isEmpty()) {
+//         qWarning() << "Nie udało się załadować certyfikatu lub plik jest pusty/nieprawidłowy.";
+//         return;
+//     }
 
-    // 5. Ustaw zaktualizowaną listę certyfikatów CA w konfiguracji SSL
-    sslConfig.setCaCertificates(caCertificates);
+//     // Możesz wziąć pierwszy lub wszystkie, jeśli twój plik .pem zawiera cały łańcuch
+//     // Dla pojedynczego certyfikatu self-signed, wystarczy pierwszy.
+//     QSslCertificate selfSignedCertificate = certificates.first();
+//     // Lub jeśli chcesz dodać wszystkie z pliku:
+//     // QList<QSslCertificate> selfSignedCertificates = certificates;
 
-    // 6. Ustaw tę konfigurację jako domyślną dla wszystkich przyszłych połączeń SSL
-    QSslConfiguration::setDefaultConfiguration(sslConfig);
 
-    qDebug() << "Konfiguracja SSL zaktualizowana o własny certyfikat.";
-}
+//     // 2. Pobierz domyślną konfigurację SSL
+//     QSslConfiguration sslConfig = QSslConfiguration::defaultConfiguration();
+
+//     // 3. Pobierz listę aktualnie zaufanych certyfikatów CA
+//     QList<QSslCertificate> caCertificates = sslConfig.caCertificates();
+
+//     // 4. Dodaj swój certyfikat do listy zaufanych CA
+//     // Jeśli używasz QList<QSslCertificate> selfSignedCertificates:
+//     // caCertificates.append(selfSignedCertificates);
+//     // Dla pojedynczego certyfikatu:
+//     caCertificates.append(selfSignedCertificate);
+
+//     // 5. Ustaw zaktualizowaną listę certyfikatów CA w konfiguracji SSL
+//     sslConfig.setCaCertificates(caCertificates);
+
+//     // 6. Ustaw tę konfigurację jako domyślną dla wszystkich przyszłych połączeń SSL
+//     QSslConfiguration::setDefaultConfiguration(sslConfig);
+
+//     qDebug() << "Konfiguracja SSL zaktualizowana o własny certyfikat.";
+// }
 
 
 
@@ -112,16 +112,16 @@ int main(int argc, char *argv[])
 
 
 
-    // Przykład użycia funkcji
-    QString certPath = "/home/rafal/fastapi_ssl/server.crt"; // Podaj ścieżkę do certyfikatu
+    // // Przykład użycia funkcji
+    // QString certPath = "/home/rafal/fastapi_ssl/server.crt"; // Podaj ścieżkę do certyfikatu
 
-    if (!setGlobalSslCertificate(certPath)) {
-        qDebug() << "Nie udało się ustawić globalnej konfiguracji SSL.";
-        return -1;
-    }
+    // if (!setGlobalSslCertificate(certPath)) {
+    //     qDebug() << "Nie udało się ustawić globalnej konfiguracji SSL.";
+    //     return -1;
+    // }
 
 
-    skonfigurujZaufanyCertyfikat();
+    // skonfigurujZaufanyCertyfikat();
 
 
 
