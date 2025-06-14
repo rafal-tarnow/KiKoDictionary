9409c841adf7d42b358c4c3b0b4aba830c32cf99
Rafal Kruszyna <rafal.kruszyna@gmail.com>
2025-06-14 10:17:33 +0200

    działaja testy asynchroniczne

--- KIKOFastapiServer/my-users-microservice/tests/test_main.py
+++ KIKOFastapiServer/my-users-microservice/tests/test_main.py
@@ -1,15 +1,15 @@
-from fastapi.testclient import TestClient
 import pytest
-from src.main import app
+from src.db.models.user import User
+from sqlalchemy.future import select
 
-client = TestClient(app)
-
-def test_not_found():
+@pytest.mark.asyncio
+async def test_not_found(client):
     response = client.get("/invalid-endpoint")
     assert response.status_code == 404
     assert response.json() == {"detail": "Not Found"}
-    
-def test_get_root():
+
+@pytest.mark.asyncio
+async def test_get_root(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.headers["content-type"] == "application/json"
@@ -18,12 +18,13 @@ def test_get_root():
 @pytest.fixture
 def valid_register_data():
     return {
-        "email" : "user2@example.com",
-        "username" : "john2_doe",
-        "password" : "secure123"
+        "email": "user2@example.com",
+        "username": "john2_doe",
+        "password": "secure123"
     }
 
-def test_post_register_user(valid_register_data):
+@pytest.mark.asyncio
+async def test_post_register_user(client, valid_register_data):
     response = client.post("/api/v1/auth/register", json=valid_register_data)
     assert response.status_code == 201
     assert response.headers["content-type"] == "application/json"
@@ -32,6 +33,8 @@ def test_post_register_user(valid_register_data):
     assert response_json["username"] == valid_register_data["username"]
     assert response_json["email"] == valid_register_data["email"]
 
+
+@pytest.mark.asyncio
 @pytest.mark.parametrize("invalid_email", [
     "invalid-email",
     "user@",
@@ -40,8 +43,7 @@ def test_post_register_user(valid_register_data):
     "user.domain.com",
     "user@.com"
 ])
-
-def test_post_register_user_invalid_email(valid_register_data, invalid_email):
+async def test_post_register_user_invalid_email(client, valid_register_data, invalid_email):
     invalid_data = valid_register_data.copy()
     invalid_data["email"] = invalid_email
     response = client.post("/api/v1/auth/register", json=invalid_data)
@@ -61,14 +63,15 @@ def test_post_register_user_invalid_email(valid_register_data, invalid_email):
         for error in errors
     )
 
+
+@pytest.mark.asyncio
 @pytest.mark.parametrize("invalid_username, expected_error_msg", [
     ("", "Value error, The username cannot be empty or consist only of whitespace."),
     ("   ", "Value error, The username cannot be empty or consist only of whitespace."),
     ("  john", "Value error, The username cannot contain spaces at the beginning or end."),
     ("john  ", "Value error, The username cannot contain spaces at the beginning or end."),
 ])
-
-def test_post_register_user_invalid_username(valid_register_data, invalid_username, expected_error_msg):
+async def test_post_register_user_invalid_username(client, valid_register_data, invalid_username, expected_error_msg):
     invalid_data = valid_register_data.copy()
     invalid_data["username"] = invalid_username
     response = client.post("/api/v1/auth/register", json=invalid_data)
