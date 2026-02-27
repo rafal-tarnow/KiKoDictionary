361d0d6733b78c71d647ed61518fa67954591455
Tomasz Pecak <tom85.pecak@gmail.com>
2026-02-27 18:38:40 +0100

    add sentence doesent edit language code

--- client/maia_flutter_client/README.md
+++ client/maia_flutter_client/README.md
@@ -16,7 +16,7 @@ For help getting started with Flutter development, view the
 samples, guidance on mobile development, and a full API reference.
 
 ## Run release version on android
-
+w prawym dolnym rogu VSC wybrac platforme docelowa a nastepnie:
 flutter run --release
 
 ## Build web version
--- client/maia_flutter_client/dump_project.py
+++ client/maia_flutter_client/dump_project.py
@@ -5,8 +5,9 @@ allowed_extensions = ['.dart']
 
 # Lista zablokowanych katalogów
 #blocked_dirs = ['venv', '__pycache__', '.git', 'migrations']
-blocked_dirs = ['venv', '__pycache__', '.git', 'migrations', 'dictionary' , 'health' , 'home' , 'sentences' , 'test' , 'words']
+# blocked_dirs = ['venv', '__pycache__', '.git', 'migrations', 'dictionary' , 'health' , 'home' , 'sentences' , 'test' , 'words']
 #blocked_dirs = ['venv', '__pycache__', '.git', 'migrations', 'dictionary'  , 'sentences' , 'test']
+blocked_dirs = ['venv', '__pycache__', '.git', 'migrations', 'auth', 'captcha', 'dictionary' , 'health' , 'home'  , 'settings',  'test' , 'words', ]
 
 # Lista zablokowanych plików (pełne nazwy plików)
 blocked_files = ['dump_output.txt', 'dump_project.py', '.gitignore', 'alembic.ini', '__init__.py']
